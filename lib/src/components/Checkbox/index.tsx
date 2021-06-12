10fd52ae4c05895debf9b00d86834c14845f5bb0
Fran García <fran17901@gmail.com>
2021-06-13 01:51:02 +0200

    Checkbox styles in Root checkbox

--- lib/src/components/Checkbox/index.tsx
+++ lib/src/components/Checkbox/index.tsx
@@ -1,7 +1,5 @@
 import * as Label from "@radix-ui/react-label";
 import { Indicator } from "@radix-ui/react-checkbox";
-import { useLabelContext } from "@radix-ui/react-label";
-// import { CheckIcon } from "@radix-ui/react-icons";
 import * as React from "react";
 import { keyframes, styled } from "../../config/stitches";
 import { StyledCheckbox } from "./styled";
@@ -22,30 +20,6 @@ const line2Animation = keyframes({
   "100%": {
     height: "100%",
   },
-});
-
-const containerAnimation = keyframes({
-  "0%": {
-    opacity: 0,
-    width: 0,
-    height: 0,
-  },
-  "100%": {
-    opacity: 1,
-    width: "calc(100% + 4px)",
-    height: "calc(100% + 4px)",
-  },
-});
-
-const Container = styled("div", {
-  backgroundColor: "blue",
-  position: "absolute",
-  borderRadius: "32%",
-  border: "2px solid blue",
-  animation: `${containerAnimation}`,
-  animationFillMode: "forwards",
-  animationDuration: "300ms",
-  zIndex: 0,
 });
 
 const ContainerCheckIcon = styled("div", {
@@ -90,26 +64,30 @@ const LineCheck2 = styled("span", {
 });
 
 const CheckIcon = () => (
-  <Container>
-    <ContainerCheckIcon>
-      <LineCheck1 />
-      <LineCheck2 />
-    </ContainerCheckIcon>
-  </Container>
+  <ContainerCheckIcon>
+    <LineCheck1 />
+    <LineCheck2 />
+  </ContainerCheckIcon>
 );
 
 const StyledLabel = styled(Label.Root, {
-  display: "flex",
-  alignItems: "center",
-  transition: "all .3s ease",
   "&:hover": {
-    "& button": {
+    '& [data-state="unchecked"]': {
       backgroundColor: "#eee",
-      '&[data-state="checked"]': {
-        boxShadow: "0 3px 15px 0 rgba(26, 93, 255,.35)",
-      },
+    },
+    '& [data-state="checked"]': {
+      boxShadow: "0 3px 15px 0 rgba(26, 93, 255,.35)",
     },
     cursor: "pointer",
+  },
+  variants: {
+    hasLabel: {
+      true: {
+        display: "flex",
+        alignItems: "center",
+        width: "fit-content",
+      },
+    },
   },
 });
 
@@ -119,7 +97,7 @@ const ChildrenLabel = styled("div", {
 
 export const Checkbox: React.FC = ({ children }) => {
   return (
-    <StyledLabel>
+    <StyledLabel hasLabel={!!children}>
       <StyledCheckbox>
         <Indicator as={CheckIcon} />
       </StyledCheckbox>
--- lib/src/components/Checkbox/styled.ts
+++ lib/src/components/Checkbox/styled.ts
@@ -1,5 +1,31 @@
 import { Root } from "@radix-ui/react-checkbox";
-import { styled } from "../../config/stitches";
+import { keyframes, styled } from "../../config/stitches";
+
+const fadeIn = keyframes({
+  "0%": {
+    opacity: 0,
+    width: 0,
+    height: 0,
+  },
+  "100%": {
+    opacity: 1,
+    width: "100%",
+    height: "100%",
+  },
+});
+
+const fadeOut = keyframes({
+  "0%": {
+    opacity: 1,
+    width: "100%",
+    height: "100%",
+  },
+  "100%": {
+    opacity: 0,
+    width: 0,
+    height: 0,
+  },
+});
 
 export const StyledCheckbox = styled(Root, {
   appearance: "none",
@@ -7,14 +33,31 @@ export const StyledCheckbox = styled(Root, {
   padding: 0,
   width: 23,
   height: 23,
-  display: "flex",
-  alignItems: "center",
-  justifyContent: "center",
   border: "2px solid #ccc",
   borderRadius: "32%",
   position: "relative",
   transition: "all .3s ease",
   "&:hover": {
     cursor: "pointer",
+  },
+  "&:before": {
+    content: "",
+    backgroundColor: "blue",
+    position: "absolute",
+    borderRadius: "32%",
+    border: "2px solid blue",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+  },
+  "&[data-state='checked']:before": {
+    animation: `${fadeIn}`,
+    animationFillMode: "forwards",
+    animationDuration: "300ms",
+  },
+  "&[data-state='unchecked']:before": {
+    animation: `${fadeOut}`,
+    animationFillMode: "forwards",
+    animationDuration: "300ms",
   },
 });
